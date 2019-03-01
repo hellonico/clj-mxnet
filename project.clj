105f5f31fb7c246b07d78149ac9e13e5cdb52a8d
Nicolas Modrzyk <hellonico@gmail.com>
2019-03-01 10:03:40 +0900

    Version 0.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-mxnet/lein-template "0.0.1"
+(defproject clj-mxnet/lein-template "0.0.2-SNAPSHOT"
   :description "Created with lein-create-template"
   :url "http://example.com/FIXME"
   :deploy-repositories [["releases" :clojars]]
